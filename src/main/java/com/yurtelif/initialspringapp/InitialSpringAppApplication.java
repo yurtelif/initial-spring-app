7de2ecdc001a2a23f1486632b8215fdcd526cb59
elifyurt <yrtelf@gmail.com>
2020-08-16 17:55:10 +0300

    Initial commit

---
+++ src/main/java/com/yurtelif/initialspringapp/InitialSpringAppApplication.java
@@ -0,0 +1,25 @@
+package com.yurtelif.initialspringapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class InitialSpringAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InitialSpringAppApplication.class, args);
+    }
+
+    @RestController
+    @RequestMapping("/api")
+    public class ApiController {
+        @GetMapping("/greeting")
+        public String getGreeting() {
+            return "Hello World from API";
+        }
+    }
+
+}
---
+++ src/test/java/com/yurtelif/initialspringapp/InitialSpringAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yurtelif.initialspringapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InitialSpringAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
